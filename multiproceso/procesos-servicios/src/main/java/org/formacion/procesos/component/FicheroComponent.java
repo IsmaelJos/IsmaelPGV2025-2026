ffa6aa3a3902d4773e9e269f3acdf5390047a320
mackstm <IsmaelJos@github.com>
2025-10-09 17:15:38 +0100

    feat:

--- multiproceso/procesos-servicios/src/main/java/org/formacion/procesos/component/FicheroComponent.java
+++ multiproceso/procesos-servicios/src/main/java/org/formacion/procesos/component/FicheroComponent.java
@@ -1,6 +1,7 @@
 package org.formacion.procesos.component;
 
-import org.formacion.procesos.repository.IFicheroRepository;
+import org.formacion.procesos.component.interfaces.IFicheroComponent;
+import org.formacion.procesos.repository.interfaces.IAlmacenamientoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,11 +11,11 @@ public class FicheroComponent implements IFicheroComponent{
 
     @Autowired
     @Qualifier("ficheroRepository")
-    IFicheroRepository ficheroRepository;
+    IAlmacenamientoRepository ficheroRepository;
 
     @Autowired
     @Qualifier("baseDatosRepository")
-    IFicheroRepository baseDatosRepository;
+    IAlmacenamientoRepository baseDatosRepository;
 
     @Override
     public String mensaje() {
